57164dbbf4df43824225768c37ee4be2592e003e
MahmoudHamadaIbrahim <wnttlt6@gmail.com>
2026-03-14 12:22:04 +0200

    Create not found page

--- src/app/features/notfound/notfound.component.ts
+++ src/app/features/notfound/notfound.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.css',
 })
